d3766947d818f50127b959cb53398025dc43a13e
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-10-31 00:21:08 +0900

    🎨[style]: 버튼 디자인 수정

--- src/stories/atoms/translateBtn/index.tsx
+++ src/stories/atoms/translateBtn/index.tsx
@@ -1,6 +1,6 @@
 const TranslateBtn = () => {
   return (
-    <button className="border-2 border-mainColor rounded-15 px-13 py-11 font-semibold hover:bg-mainColor hover:text-white active:bg-mainColor active:text-white">English / 한국어</button>
+    <button className="border-2 border-mainColor rounded-17 px-13 py-9 font-semibold hover:bg-mainColor hover:text-white active:bg-mainColor active:text-white">English / 한국어</button>
   )
 }
 
