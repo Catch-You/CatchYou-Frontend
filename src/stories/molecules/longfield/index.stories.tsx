571a05b98d6c748d02e6bb59285e406f10280741
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2023-10-31 00:37:20 +0900

    ✨[feat]: 긴 입력 필드 (#5)
    * ✨[feat]: 번역 버튼
    
    * 🎨[style]: 버튼 디자인 수정
    
    * ✨[feat]: 스크린 헤더
    
    * ✨[feat]: 긴 입력 필드

---
+++ src/stories/molecules/longfield/index.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Longfield from '.';
+
+const meta: Meta<typeof Longfield> = {
+  title: 'molecules/Longfield',
+  component: Longfield,
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof Longfield>;
+
+export const Primary: Story = {
+  args: {
+    label: '이메일',
+    placeholder: '대소문자 구분 없이 6자 이상, 특수문자 포함'
+  },
+};
