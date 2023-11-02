263f45c3e0efab37fd4906fc6883f9fca3e4736d
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-11-02 22:52:20 +0900

    ✨[feat]: 라우팅 환경 설정

---
+++ src/routes/layoutRouter.ts
@@ -0,0 +1,12 @@
+import MontagePublicPage from "../stories/pages/montagePublic/MontagePublicPage";
+import { TRoute } from "../types/commonTypes";
+
+export const LAYOUT_ROUTES_URLS = {
+  montagePublic: {
+    name: "몽타주 공개 페이지",
+    path: () => '/',
+    component: MontagePublicPage,
+  }
+} as const;
+
+export const LAYOUT_ROUTES: TRoute[] = Object.values(LAYOUT_ROUTES_URLS);
