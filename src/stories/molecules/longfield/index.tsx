6e70008b58a2801cc1e2d40b0bc7c122a6f8b330
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-11-05 21:21:43 +0900

    #19 ✨[feat]: type 속성 추가 및 디자인 수정

--- src/stories/molecules/longfield/index.tsx
+++ src/stories/molecules/longfield/index.tsx
@@ -1,13 +1,15 @@
 export type TField = {
-  label?: string;
+  label: string;
   placeholder?: string;
+  type: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Longfield = ({label, placeholder}: TField) => {
+const Longfield = ({label, placeholder, type, onChange}: TField) => {
   return (
     <div className="flex flex-col gap-2 w-full max-w-400">
       <label>{label}</label>
-      <input placeholder={placeholder} className="border-2 border-superSubColor rounded-12 px-14 py-13"></input>
+      <input onChange={onChange} placeholder={placeholder} type={type} className="border-2 border-superSubColor rounded-12 px-14 py-10"></input>
     </div>
   );
 }
