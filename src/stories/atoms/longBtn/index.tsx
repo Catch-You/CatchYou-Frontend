724e188accde996733800e5f9108cca4c9637184
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-11-05 21:22:33 +0900

    #19 ✨[feat]: 버튼에 onClick, disabled 속성 추가

--- src/stories/atoms/longBtn/index.tsx
+++ src/stories/atoms/longBtn/index.tsx
@@ -1,11 +1,12 @@
 export type TLongBtn = {
   text: string;
-  activate?: boolean;
+  activate: boolean;
+  onClick: () => void;
 }
 
-const LongBtn = ({ text, activate }: TLongBtn) => {
+const LongBtn = ({ text, activate, onClick }: TLongBtn) => {
   return (
-    <button className={`rounded-17 font-semibold text-center text-white py-16 px-175 ${activate ? 'bg-mainColor ' : 'bg-superSubColor'}`}>{text}</button>
+    <button onClick={onClick} disabled={activate? false : true} className={`rounded-17 font-semibold text-center text-white py-14 w-full ${activate ? 'bg-mainColor' : 'bg-superSubColor'}` }>{text}</button>
   );
 }
 
--- src/stories/molecules/longfield/index.stories.tsx
+++ src/stories/molecules/longfield/index.stories.tsx
@@ -13,6 +13,6 @@ type Story = StoryObj<typeof Longfield>;
 export const Primary: Story = {
   args: {
     label: '이메일',
-    placeholder: '대소문자 구분 없이 6자 이상, 특수문자 포함'
+    placeholder: '대소문자 구분 없이 6자 이상, 특수문자 포함',
   },
 };
