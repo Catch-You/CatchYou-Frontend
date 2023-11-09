86975d131ff2b62eb6552639ea41bdfeeaceb966
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2023-11-09 15:48:42 +0900

    ✨[feat]: 회원 분류 페이지  (#26)
    * #23 ✨[feat]: 회원가입 경로 수정
    
    * #23 ✨[feat]: 회원가입 페이지
    
    * #23 ✨[feat]: 회원분류 페이지 라우터 설정

--- src/stories/organisms/signMember/index.tsx
+++ src/stories/organisms/signMember/index.tsx
@@ -5,12 +5,13 @@ import VisibilityIcon from '@mui/icons-material/Visibility';
 type TSignMember = {
   title: string;
   subtitle: string;
-  police: boolean
+  police: boolean;
+  onClick: () => void;
 }
 
-const SignMember = ({title, subtitle, police}: TSignMember) => {
+const SignMember = ({title, subtitle, police, onClick}: TSignMember) => {
   return (
-    <div className="text-center border rounded-20 p-40 w-full max-w-350 h-460 gap-2">
+    <div className="text-center border rounded-20 p-40 w-full max-w-350 h-460 gap-2 bg-white" onClick={onClick}>
       <Title text={title} />
       <div className="text-grayColor text-15 mb-70">{subtitle}</div>
       {police? <LocalPoliceIcon style={{width: 120, height:120}} /> : <VisibilityIcon style={{width: 120, height:120}} />}
