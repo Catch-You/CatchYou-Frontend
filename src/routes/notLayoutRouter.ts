3ac40655b776aa44e113196647ba9d892f666812
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-11-09 15:45:42 +0900

    #23 ✨[feat]: 회원분류 페이지 라우터 설정

--- src/routes/notLayoutRouter.ts
+++ src/routes/notLayoutRouter.ts
@@ -1,4 +1,5 @@
 import LoginPage from "../stories/pages/login/LoginPage";
+import SignMemberPage from "../stories/pages/sign/SignMemberPage";
 import SignPage from "../stories/pages/sign/SignPage";
 import { TRoute } from "../types/commonTypes";
 
@@ -8,11 +9,16 @@ export const NOT_LAYOUT_ROUTES_URLS = {
     path: () => '/login',
     component: LoginPage,
   },
+  signMemberPage: {
+    name: "회원 분류 페이지",
+    path: () => '/member',
+    component: SignMemberPage,
+  },
   signPage: {
     name: "회원가입 페이지",
     path: () => '/join',
     component: SignPage,
-  }
+  },
 } as const;
 
 export const NOT_LAYOUT_ROUTES: TRoute[] = Object.values(NOT_LAYOUT_ROUTES_URLS);
