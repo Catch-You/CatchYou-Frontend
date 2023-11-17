daf3e926036424a3438024185f47d40e666fdd8c
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-11-17 18:45:09 +0900

    #33 ✨[feat]: selector 값 받아오기

--- src/stories/molecules/selector/index.tsx
+++ src/stories/molecules/selector/index.tsx
@@ -1,12 +1,27 @@
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import { useState } from 'react';
+import { Dispatch, SetStateAction, useState } from 'react';
 
 type TSelector = {
   text: string;
   options: string[];
+  type: number;
+  caseForm: {
+    open: boolean,
+    region: string,
+    type: string,
+    impressive: string,
+    overview: string,
+  },
+  setCaseForm: Dispatch<SetStateAction<{
+    open: boolean,
+    region: string,
+    type: string,
+    impressive: string,
+    overview: string,
+  }>>
 }
 
-const Selector = ({text, options}: TSelector) => {
+const Selector = ({text, options, type, caseForm, setCaseForm }: TSelector) => {
 
   const [isOpen, setIsOpen] = useState(false);
   const [selectedOption, setSelectedOption] = useState<string>('');
@@ -17,6 +32,13 @@ const Selector = ({text, options}: TSelector) => {
 
   const handleOptionSelect = (option: string) => {
     setSelectedOption(option);
+    if (type === 0) {
+      setCaseForm({...caseForm, open: option==="공개"? true: false})
+    } else if (type === 1) {
+      setCaseForm({...caseForm, region: option})
+    } else if (type === 2) {
+      setCaseForm({...caseForm, type: option})
+    }
     setIsOpen(false);
   };
 
@@ -29,21 +51,20 @@ const Selector = ({text, options}: TSelector) => {
     <div className="flex flex-col gap-2">
       <div>{text}</div>
       <div className='relative'>
-        <select value={selectedOption} className="w-full max-w-193 rounded-12 border-2 border-superSubColor px-14 py-10 appearance-none" onClick={handleSelectClick} onChange={(e) => setSelectedOption(e.target.value)} >
-          <option value="">
+        <select className='w-193 rounded-12 border-2 border-superSubColor px-14 py-10 appearance-none' onClick={handleSelectClick} onChange={(e) => setSelectedOption(e.target.value)} >
+          <option>
             {selectedOption ? selectedOption : '옵션을 선택하세요'}
           </option>
         </select>
         <span className='absolute top-1/4 start-40 text-superSubColor cursor-pointer' onClick={handleToggle}><KeyboardArrowDownIcon /></span>
         
         {isOpen && (
-          <div className="absolute top-full left-0 z-10 w-full max-w-193 rounded-12 border-2 border-superSubColor mt-1">
-          
+          <div className="absolute top-full left-0 z-10 w-full max-w-193 rounded-12 border-2 border-superSubColor mt-1 bg-white p-3">
             {options.map((item, index) => (
               <option
                 key={`${item}+${index}`}
                 value={item}
-                className="px-10 py-6 cursor-pointer hover:bg-gray-200"
+                className="px-10 py-6 cursor-pointer hover:bg-gray-200 bg-white"
                 onClick={() => handleOptionSelect(item)}
               >
                 {item}
@@ -52,7 +73,6 @@ const Selector = ({text, options}: TSelector) => {
           </div>
         )}
       </div>
-      
     </div>
   )
 }
