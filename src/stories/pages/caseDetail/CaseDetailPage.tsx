5029e42b02da7eb7601cdbe62628e615b1917275
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-11-18 15:02:05 +0900

    #34 ✨[feat]: 몽타주 상세 페이지

---
+++ src/stories/pages/caseDetail/CaseDetailPage.tsx
@@ -0,0 +1,34 @@
+import './styles.css'
+
+const CaseDetailPage = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="bg-white  w-full max-w-748 h-full max-h-fit rounded-20 py-20 px-50 mt-30">
+        <div className="flex gap-8 mt-20">
+          <div className="montage"><div className='region'>서울</div></div>
+          <div>
+            <div className='text-mainColor font-semibold text-20'>인상착의</div>
+            <div className='mt-5 border-2 border-superSubColor w-full max-w-360 h-215 rounded-12 px-14 py-16'>
+              넌 잠에 드는 법도 모른 채 밤을 걷지
+              언제부터 넌 너를 속이고 밝게 웃지
+              아무도 너의 슬픔을 알아주지 않지
+              눈물을 감추기엔 네가 가여워서 못 버텨
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className='text-mainColor font-semibold text-20 mt-20'>사건개요</div>
+          <div className='mt-5 border-2 border-superSubColor w-full h-140 rounded-12 px-14 py-16'>
+            넌 잠에 드는 법도 모른 채 밤을 걷지
+            언제부터 넌 너를 속이고 밝게 웃지
+            아무도 너의 슬픔을 알아주지 않지
+            눈물을 감추기엔 네가 가여워서 못 버텨
+          </div>
+        </div>
+        <div className="flex justify-end"><button className="text-white bg-mainColor px-35 py-10 rounded-16 my-20">수정하기</button></div>
+      </div>
+    </div>
+  )
+}
+
+export default CaseDetailPage;
