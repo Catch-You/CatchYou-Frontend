9e97fba65f037a0fdb38f6221bcf413b9e41ef49
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-11-18 15:00:18 +0900

    #34 ✨[feat]: 몽타주 카드 클릭시 상세 페이지로 이동

--- src/stories/atoms/infoCard/index.tsx
+++ src/stories/atoms/infoCard/index.tsx
@@ -1,12 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
 type TInfoCard = {
   name: string;
   age?: number;
   imgSrc: string;
+  id: number;
 }
 
-const InfoCard = ({name, age, imgSrc}: TInfoCard) => {
+const InfoCard = ({name, age, imgSrc, id}: TInfoCard) => {
+  const navigate = useNavigate();
+  const handleClick = ()  => {
+    navigate(`/${id}`)
+  }
   return (
-    <div className="h-240 w-190 rounded-lg bg-white flex flex-col">
+    <div className="h-240 w-190 rounded-lg bg-white flex flex-col" onClick={handleClick}>
       <img className="rounded-t-lg" src={imgSrc} width={230} height={200} />
       <div className="text-center text-16 pt-10">{name} / {age? `${age}`: '?'}세</div>
     </div>
