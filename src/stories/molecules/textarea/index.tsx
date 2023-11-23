921d1f73f277f3c81d6d2fa77d0e8072860fc7c6
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-11-23 17:24:08 +0900

    #39 🐛[fix]: props 옵션 및 조건문 수정

--- src/stories/molecules/textarea/index.tsx
+++ src/stories/molecules/textarea/index.tsx
@@ -6,28 +6,29 @@ type TTextArea = {
   placeholder: string;
   rows: number;
   maxInput: number;
-  type: number;
-  caseForm: TCaseForm,
-  setCaseForm: Dispatch<SetStateAction<TCaseForm>>;
+  bgColor?: boolean;
+  type?: number;
+  caseForm?: TCaseForm,
+  setCaseForm?: Dispatch<SetStateAction<TCaseForm>>;
 }
 
-const TextArea = ({text, rows, maxInput, placeholder, type, caseForm, setCaseForm}: TTextArea) => {
+const TextArea = ({text, rows, maxInput, placeholder, type, bgColor, caseForm, setCaseForm}: TTextArea) => {
   const [inputCount, setInputCount] = useState(0);
 
 const onTextareaHandler = (input: string) => {
     setInputCount(
-      input.replace(/[\0-\x7f]|([0-\u07ff]|(.))/g, "$&$1$2").length
+      input.replace(/[\0-\x7f]|([0-\u07ff]|(.))/g, "$&").length
     );
-    if (type === 0) {
+    if (caseForm && setCaseForm && type === 0) {
       setCaseForm({...caseForm, impressive: input})
-    } else if (type === 1) {
+    } else if (caseForm && setCaseForm && type === 1) {
       setCaseForm({...caseForm, overview: input})
     }
     
   };
   return (
     <>
-      <div className="flex justify-between">{text} <span className="text-superSubColor font-semibold">({inputCount}/{maxInput})</span></div>
+      <div className="flex justify-between">{text} <span className={bgColor? "text-subColor ": "text-superSubColor"+" font-semibold"}>({inputCount}/{maxInput})</span></div>
       <textarea placeholder={placeholder} maxLength={maxInput} rows={rows} className="w-full rounded-12 border-2 border-superSubColor px-14 py-10 mt-8" onChange={(e) => onTextareaHandler(e.target.value)}>
       </textarea>
     </>
