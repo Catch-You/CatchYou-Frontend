86b7306cc1235e669d9a1e8513644e1ccee23cc6
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-12-04 01:47:32 +0900

    #45 🐛[fix]: onClick props 추가

--- src/stories/atoms/shortBtn/index.tsx
+++ src/stories/atoms/shortBtn/index.tsx
@@ -1,8 +1,8 @@
 import { TLongBtn } from "../longBtn";
 
-const ShortBtn = ({text, activate, disabled}: TLongBtn) => {
+const ShortBtn = ({ text, activate, disabled, onClick }: TLongBtn) => {
   return (
-    <button disabled={disabled} className={`flex items-center justify-center rounded-12 font-semibold text-center text-white w-80 py-12 ${activate ? 'bg-mainColor ' : 'bg-superSubColor'}`}>{text}</button>
+    <button onClick={onClick} disabled={disabled} className={`flex items-center justify-center rounded-12 font-semibold text-center text-white w-80 py-12 ${activate ? 'bg-mainColor ' : 'bg-superSubColor'}`}>{text}</button>
   )
 }
 
