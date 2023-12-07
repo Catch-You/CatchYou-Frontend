76b50c31af5e453acb9130ac7b96010c1efd6acc
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-12-07 15:50:34 +0900

    ✨[feat]: 기획 수정에 따른 목데이터 세부 데이터 삭제

--- src/stories/atoms/infoCard/index.tsx
+++ src/stories/atoms/infoCard/index.tsx
@@ -1,21 +1,19 @@
 import { useNavigate } from "react-router-dom";
 
 type TInfoCard = {
-  name: string;
-  age?: number;
   imgSrc: string;
   id: number;
 }
 
-const InfoCard = ({name, age, imgSrc, id}: TInfoCard) => {
+const InfoCard = ({imgSrc, id}: TInfoCard) => {
   const navigate = useNavigate();
   const handleClick = ()  => {
     navigate(`/${id}`)
   }
   return (
-    <div className="h-240 w-190 rounded-lg bg-white flex flex-col" onClick={handleClick}>
-      <img className="rounded-t-lg" src={imgSrc} width={230} height={200} />
-      <div className="text-center text-16 pt-10">{name} / {age? `${age}`: '?'}세</div>
+    <div className="w-200 h-200 rounded-lg bg-white flex flex-col" onClick={handleClick}>
+      <img className="rounded-lg object-fill" src={imgSrc} width={200} height={200} />
+      {/* {name && age && (<div className="text-center text-16 pt-10">{name} / {age? `${age}`: '?'}세</div>)} */}
     </div>
   )
 }
