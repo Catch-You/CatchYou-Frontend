93f501f641b999975e2a8a0d3d7d5dcd81694a1b
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-12-09 17:26:58 +0900

    #50 ✨[feat]: 회원 권한 설정 로직

--- src/stories/pages/sign/SignMemberPage.tsx
+++ src/stories/pages/sign/SignMemberPage.tsx
@@ -1,16 +1,22 @@
 import { useNavigate } from "react-router-dom";
 import SignMember from "../../organisms/signMember";
+import { userRoleState } from "../../../recoil";
+import { useRecoilState } from "recoil";
 
 const SignMemberPage = () => {
   const navigate = useNavigate();
-  const handleJoinClick = () => {
+  const [, setUserRole] = useRecoilState(userRoleState); 
+
+  const handleJoinClick = (police: boolean) => {
+    const role = police ? "ROLE_POLICE" : "ROLE_DIRECTOR";
+    setUserRole(role)
     navigate('/join')
   }
   return(
     <div className="justify-center items-center ">
       <div className="flex justify-center mt-100 gap-12">
-      <SignMember title="경찰" subtitle="과학수사관리관, 수사국, 형사국" police={true} onClick={handleJoinClick} />
-      <SignMember title="몽타주 전문가" subtitle="몽타주 제작 전문가" police={false} onClick={handleJoinClick} />
+        <SignMember title="경찰" subtitle="과학수사관리관, 수사국, 형사국" police={true} onClick={() => handleJoinClick(true)} />
+        <SignMember title="몽타주 전문가" subtitle="몽타주 제작 전문가" police={false} onClick={() => handleJoinClick(true)} />
       </div>
     </div>
   )
