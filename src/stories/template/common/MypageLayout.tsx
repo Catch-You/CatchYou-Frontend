1902948a483e2c58056df7b56a6a2cb78d96566e
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-12-17 08:18:00 +0900

    #66 ✨[feat]: 마이페이지 권한 별 사이드 바 설정

--- src/stories/template/common/MypageLayout.tsx
+++ src/stories/template/common/MypageLayout.tsx
@@ -1,12 +1,17 @@
 import { HTMLAttributes, ReactNode } from "react";
 import Header from "../../molecules/header"
 import { Outlet } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { userInfoState } from "../../../recoil";
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
   children: ReactNode;
 }
 
 const MypageLayout: React.FC<Props> = ({ ...props }) => {
+  const userInfo = useRecoilValue(userInfoState); 
+  const role = userInfo.role === 'ROLE_POLICE' ? '경찰':'몽타주 전문가'
+  
   return (
     <div className="max-w-1440 min-h-screen h-full flex flex-col" {...props}>
       <Header />
@@ -14,9 +19,17 @@ const MypageLayout: React.FC<Props> = ({ ...props }) => {
         <div className="bg-white h-full min-h-600 px-40 mt-20 rounded-20 ">
           <div className="flex py-20 w-full">
             <div className="mt-35 w-250">
-              <div className="text-20 font-semibold">몽타주 전문가(홍길동)</div>
+              <div className="text-20 font-semibold">{role} / {userInfo.userName}님</div>
               <div className="h-2 bg-superSubColor w-full mt-5"></div>
-              <div className="mt-15">관리사건</div>
+              {role === '경찰'? (
+                <div className="mt-20 flex flex-col gap-4">
+                  <div>관리사건</div>
+                  <a href='/registerCase'>사건등록</a>
+                </div>) : 
+                (<div className="mt-20 flex flex-col gap-4">
+                  <div>관리사건</div>
+                  <a href='/createMontage'>몽타주 생성</a>
+                </div>)}
             </div>
             <div className="h-full min-h-580 w-2 bg-superSubColor ml-20"> </div>
             <Outlet />
