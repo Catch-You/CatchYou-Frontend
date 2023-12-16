beadd163dfeff2a7adef52a9cbe6a67ee377964d
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-12-17 08:18:57 +0900

    #66 ✨[feat]: 사건 상세 조회 api

--- src/api/caseApi.ts
+++ src/api/caseApi.ts
@@ -1,13 +1,21 @@
 import { $axios } from "../lib/axios";
-import { TCaseRegist } from "../types/case/caseManage";
+import { TCaseDetail, TCaseRegist } from "../types/case/caseManage";
+import { TApiResponse } from "../types/commonTypes";
 
 const CASE_QUERY_KEYS = {
   CASE_REGIST: () => '/criminal/police',
+  CASE_DETAIL: (id: number) => `/criminal/police/${id}`
 } as const;
 
 // 사건 등록
 export const postCase = async (params: TCaseRegist) => {
   const res = await $axios.post(CASE_QUERY_KEYS.CASE_REGIST(), params);
   console.log("Ddd",res.status)
+  return res.data;
+}
+
+// 사건 상세 조회
+export const getCaseDetail = async (id: number) => {
+  const res = await $axios.get<TApiResponse<TCaseDetail>>(CASE_QUERY_KEYS.CASE_DETAIL(id))
   return res.data;
 }
--- src/types/case/caseManage.ts
+++ src/types/case/caseManage.ts
@@ -6,3 +6,23 @@ export type TCaseRegist = {
   region: string;
   crimeType: string;
 }
+
+// 사건 상세 Response
+export type TCaseDetail = {
+  id: number;
+  userId: number;
+  userName: string;
+  title: string;
+  summary: string;
+  description: string;
+  criminalCode: string;
+  region: string;
+  crimeType: string;
+  status: string;
+  selectStatus: string;
+  createdAt: string;
+  updatedAt?: string;
+  montageId?: number;
+  directorId?: number;
+  directorName?: string;
+}
