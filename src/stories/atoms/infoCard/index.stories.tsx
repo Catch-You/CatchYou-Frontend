8fcc429b83a6aa57ed37fccbf0da7c51cfb1b9a8
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-12-17 21:21:44 +0900

    #59 🐛[bug]: 카드 빌드에러 수정

--- src/stories/atoms/infoCard/index.stories.tsx
+++ src/stories/atoms/infoCard/index.stories.tsx
@@ -12,8 +12,5 @@ type Story = StoryObj<typeof InfoCard>;
 
 export const Primary: Story = {
   args: {
-    name: '홍길동',
-    imgSrc: 'https://blogpfthumb-phinf.pstatic.net/MjAyMzA4MTVfMjgx/MDAxNjkyMDM0MjE3MzQ4._BVCNU9m5pBQ6jQzxHRkRwNtiuKck6RwevKeji6AcmMg.mCqu36OBQR7mHkS4uKd7_9hFOCo7JqkwpPWiFICjhucg.JPEG.myday0827/IMG_0137.jpg/IMG_0137.jpg?type=w161',
-    id: 1
   },
 };
