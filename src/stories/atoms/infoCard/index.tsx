da4a1733a7e32c187a267f3d2d943eb214c36059
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-12-17 21:16:10 +0900

    #59 ✨[feat]: 몽타주 카드 UI 업데이트

--- src/stories/atoms/infoCard/index.tsx
+++ src/stories/atoms/infoCard/index.tsx
@@ -1,19 +1,20 @@
 import { useNavigate } from "react-router-dom";
 
 type TInfoCard = {
-  imgSrc: string;
-  id: number;
+  montageId: number;
+  caseId: number;
+  title: string;
 }
 
-const InfoCard = ({imgSrc, id}: TInfoCard) => {
+const InfoCard = ({montageId, caseId, title}: TInfoCard) => {
   const navigate = useNavigate();
   const handleClick = ()  => {
-    navigate(`/${id}`)
+    navigate(`/${caseId}`)
   }
   return (
-    <div className="w-200 h-200 rounded-lg bg-white flex flex-col" onClick={handleClick}>
-      <img className="rounded-lg object-fill" src={imgSrc} width={200} height={200} />
-      {/* {name && age && (<div className="text-center text-16 pt-10">{name} / {age? `${age}`: '?'}세</div>)} */}
+    <div className="w-200 h-200 rounded-lg bg-white flex flex-col mb-50" onClick={handleClick}>
+      <img className="rounded-t-lg object-fill" src={`https://diffusion-ml.s3.ap-northeast-2.amazonaws.com/${montageId}.png`} width={200} height={200} />
+      {title && (<div className="text-center text-16 py-8 bg-white rounded-b-lg">{title}</div>)}
     </div>
   )
 }
