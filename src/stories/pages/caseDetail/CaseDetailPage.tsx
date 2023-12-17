f92705f9ac75cdb53300edd8b89a389a5456dd1a
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-12-17 22:44:26 +0900

    #60 ✨[feat]: 공개 몽타주 상세페이지

--- src/stories/pages/caseDetail/CaseDetailPage.tsx
+++ src/stories/pages/caseDetail/CaseDetailPage.tsx
@@ -1,40 +1,51 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import CommonBtn from '../../atoms/commonBtn';
 import './styles.css'
+import { useGetMontageDetail } from '../../../hooks/queries/montage/montageQueries';
 
 const CaseDetailPage = () => {
 
   const navigate = useNavigate();
-  // const id: number = 1;
+  const { id: caseId } = useParams();
+
+  //server
+  const { data: caseDetail } = useGetMontageDetail(Number(caseId));
+
+  // 작성날짜만을 뽑아내는 정규식
+  const createDate = (date: string) => {
+    const match = date.match(/^(\d{4}-\d{2}-\d{2})/);
+    return match && match[1];
+  }
 
   return (
     <div className="flex justify-center">
+      {caseDetail && (
       <div className="bg-white  w-full max-w-748 h-full max-h-fit rounded-20 py-20 px-50 mt-30">
-        <div className='flex items-center gap-4 mt-20'><div className='text-mainColor font-semibold text-20 bg-superSubColor w-fit px-20 py-4 rounded-15'>어금니 아빠 사건</div><span className='mt-5 text-mainColor'>| 서울</span></div>
+        <div className='flex items-center justify-between mt-20'>
+          <div className='flex gap-4'>
+            <div className='text-mainColor font-semibold text-19 bg-superSubColor w-fit px-20 py-4 rounded-15'>{caseDetail.title}</div>
+            <span className='mt-5 text-mainColor'>| {caseDetail.region}</span>
+          </div>
+          <span className="flex justify-end text-mainColor text-15">작성일: {createDate(caseDetail.updatedAt)}</span>
+        </div>
         
         <div className="flex gap-8 mt-20">
-          <div className="montage"><div className='region'>강간</div></div>
+          <div className="montage" style={{ backgroundImage: `url(https://diffusion-ml.s3.ap-northeast-2.amazonaws.com/${caseDetail.montageId}.png)` }}><div className='criminalType'>{caseDetail.crimeType}</div></div>
           <div>
             <div className='text-mainColor font-semibold text-20'>인상착의</div>
-            <div className='mt-5 border-2 border-superSubColor w-full max-w-360 h-215 rounded-12 px-14 py-16'>
-              넌 잠에 드는 법도 모른 채 밤을 걷지
-              언제부터 넌 너를 속이고 밝게 웃지
-              아무도 너의 슬픔을 알아주지 않지
-              눈물을 감추기엔 네가 가여워서 못 버텨
+            <div className='mt-5 border-2 border-superSubColor w-360 h-215 rounded-12 px-14 py-16'>
+              {caseDetail.description}
             </div>
           </div>
         </div>
         <div>
           <div className='text-mainColor font-semibold text-20 mt-20'>사건개요</div>
           <div className='mt-5 border-2 border-superSubColor w-full h-140 rounded-12 px-14 py-16'>
-            넌 잠에 드는 법도 모른 채 밤을 걷지
-            언제부터 넌 너를 속이고 밝게 웃지
-            아무도 너의 슬픔을 알아주지 않지
-            눈물을 감추기엔 네가 가여워서 못 버텨
+            {caseDetail.summary}
           </div>
         </div>
         <div className="flex justify-end mt-10"><CommonBtn text="수정하기" onClick={() => navigate(`modifyCase`)} /></div>
-      </div>
+      </div>)}
     </div>
   )
 }
