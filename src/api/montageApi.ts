99db161a3abdcccc9d23d7fae00355bf63f61a67
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-12-17 22:43:16 +0900

    #60 ✨[feat]: 공개 몽타주 상세조회 api

--- src/api/montageApi.ts
+++ src/api/montageApi.ts
@@ -1,12 +1,20 @@
 import { $axios } from "../lib/axios";
 import { TApiResponse } from "../types/commonTypes";
-import { TMontagePublic } from "../types/montage/montagePublic";
+import { TMontageDetail, TMontagePublic } from "../types/montage/montagePublic";
 
 const MONTAGE_QUERY_KEYS = {
   MONTAGE_PUBLIC: (region: string = "서울") => `/criminal/open?region=${region || "서울"}`,
+  MONTAGE_DETAIL: (caseId: number) => `/criminal/open/${caseId}`
 } as const;
 
+// 지역별 공개 몽타주 조회
 export const getMontagePublic = async (region: string) => {
   const res = await $axios.get<TApiResponse<TMontagePublic>>(MONTAGE_QUERY_KEYS.MONTAGE_PUBLIC(region))
   return res.data;
+}
+
+// 공개 몽타주 상세 조회 
+export const getMontageDetail = async (caseId: number) => {
+  const res = await $axios.get<TMontageDetail>(MONTAGE_QUERY_KEYS.MONTAGE_DETAIL(caseId))
+  return res;
 }
