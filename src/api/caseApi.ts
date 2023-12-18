3544d4246481811f7f7cd824988928a4eb861e74
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-12-18 17:25:40 +0900

    #65 ✨[feat]: 사건 등록 api

--- src/api/caseApi.ts
+++ src/api/caseApi.ts
@@ -2,17 +2,22 @@ import { $axios } from "../lib/axios";
 import { TCaseDetail, TCaseRegist } from "../types/case/caseManage";
 import { TApiResponse } from "../types/commonTypes";
 
+
 const CASE_QUERY_KEYS = {
   CASE_REGIST: () => '/criminal/police',
   CASE_DETAIL: (id: number) => `/criminal/police/${id}`
 } as const;
 
 // 사건 등록
-export const postCase = async (params: TCaseRegist) => {
-  const res = await $axios.post(CASE_QUERY_KEYS.CASE_REGIST(), params);
-  console.log("Ddd",res.status)
-  return res.data;
-}
+export const postCase = async (params: TCaseRegist, auth: string) => {
+  console.log("함부자", auth);
+  return $axios.post(CASE_QUERY_KEYS.CASE_REGIST(), params, {
+    headers: {
+      Authorization: `Bearer ${auth}`,
+    },
+  });
+};
+
 
 // 사건 상세 조회
 export const getCaseDetail = async (id: number) => {
