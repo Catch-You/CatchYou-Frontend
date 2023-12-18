695547dd249c619eb98dbffed717ca6bf136a37a
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-12-18 21:07:57 +0900

    #74 ✨[feat]: 사건 코드 확인 api

--- src/hooks/queries/case/caseQueries.ts
+++ src/hooks/queries/case/caseQueries.ts
@@ -3,6 +3,7 @@ import { getMyCaseList } from "../../../api/caseApi";
 
 export const CASE_QUERY_KEYS = {
   myCase: (role: string) => ["myCaseList", role],
+  codeCheck: (code: string) => ["codeCheck", code]
 } as const;
 
 // 내가 담당한 사건 리스트 조회
